2c525a20ec4760f4b8fd7e13b4028e30d4539b6b
MAZA2 <mahmoud.zakria@mondiamedia.com>
2017-10-22 14:51:35 +0200

    added the smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 var header = require('./header.js');
 var stars = require('./stars.js');
+var scroll = require('./scroll.js')
 
 window.addEventListener('load', function(){
 	// stars();
+	scroll()
 	header.headerTextSlider();
 	header.headerNav()
 });
